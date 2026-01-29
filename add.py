2cffcc00dcd0eedd3c2ae86595354b0418f1f92f
viswanthan2004 <viwanthannm@gmail.com>
2026-01-29 14:53:16 +0530

    Initial commit

---
+++ add.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 29 14:19:28 2026
+
+@author: AIL16
+"""
+
+# Input two numbers
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+# Add the numbers
+sum = a + b
+
+# Display the result
+print("Sum =", sum)
